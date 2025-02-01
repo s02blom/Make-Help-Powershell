841d8043ee8d3b623fd29660199258578d9ba4da
Sofia BLom - Ender <sofia@familjenblom.se>
2025-02-01 12:59:04 +0100

    Document first results

--- poweshell_version.ps1
+++ poweshell_version.ps1
@@ -1,1 +1,7 @@
-@powershell -NoProfile -Command "Get-Content $(MAKEFILE_LIST) | Select-String '^[a-zA-Z_-]+:.*?## .*$$' | ForEach-Object { Write-Host ('{0,-20} {1}' -f ($_ -split '(:)')[1], ($_ -split ':.*?## ')[0]) -ForegroundColor Cyan }"
+# Version one
+Get-Content -path "makefile" | Select-String '^[a-zA-Z_-]+:.*?## .*$$' | ForEach-Object { Write-Host ('{0,-20} {1}' -f ($_ -split '(:)')[1], ($_ -split ':.*?## ')[0]) -ForegroundColor Cyan }
+# Displayed the following
+#:      hello
+#:      help
+#:      world
+
